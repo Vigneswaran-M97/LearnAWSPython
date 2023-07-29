aff7a74662f19708571cdc241fea51d8a97708e8
Vigneswaran <75010821+Vigneswaran-M97@users.noreply.github.com>
2023-07-29 22:22:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome to the Flask Welcome API!'
+    return 'Welcome to the Flask Welcome API Test!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
